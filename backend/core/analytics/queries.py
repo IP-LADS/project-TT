c2e13fa67dac4a1af4c3df27c38ac3cc4c2332f4
Jo Schlemper <jschlemper@Jos-MacBook-Pro.local>
2019-11-29 15:48:29 -0500

    add queries in analytics module

---
+++ backend/core/analytics/queries.py
@@ -0,0 +1,30 @@
+""" API's"""
+
+
+def find_most_liked_media(dataset):
+    pass
+
+
+def find_most_commented_media(dataset):
+    pass
+
+
+def find_media_by_tag(dataset, tag):
+    pass
+
+
+def find_media_by_tags(dataset, tags):
+    pass
+
+
+def find_media_by_date(dataset, date):
+    pass
+
+
+def find_top_k_liked_media(dataset):
+    pass
+
+
+def sort_dataset(datset):
+    """ sort the dataset by number of likes """
+    pass
